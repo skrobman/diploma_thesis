501ec8ae0edaaae27f6cdc53746fcb06d307b989
Mikhail Skrobat <obgora228@gmail.com>
2025-11-14 22:05:17 +0100

    .env changes

--- app/config/auth.py
+++ app/config/auth.py
@@ -7,12 +7,12 @@ load_dotenv()
 DEFAULT_EXPIRES_IN_SECONDS = 604800
 expires_int = DEFAULT_EXPIRES_IN_SECONDS
 
-try:
-    expires_str = os.getenv("JWT_REFRESH_TOKEN_EXPIRES")
-    if expires_str is not None:
-        expires_int = int(expires_str)
-except (ValueError, TypeError):
-    pass
+# try:
+#     expires_str = os.getenv("JWT_REFRESH_TOKEN_EXPIRES")
+#     if expires_str is not None:
+#         expires_int = int(expires_str)
+# except (ValueError, TypeError):
+#     pass
 
 secret_key = os.getenv("JWT_SECRET_KEY")
 if not secret_key:
@@ -21,7 +21,8 @@ if not secret_key:
 config = AuthXConfig(
     JWT_SECRET_KEY=secret_key,
     JWT_ALGORITHM="HS256",
-    JWT_REFRESH_TOKEN_EXPIRES=expires_int,
+    JWT_REFRESH_TOKEN_EXPIRES=os.getenv("JWT_REFRESH_TOKEN_EXPIRES", "7d"),
+    #JWT_REFRESH_TOKEN_EXPIRES=expires_int,
     JWT_COOKIE_CSRF_PROTECT=False
 )
 
